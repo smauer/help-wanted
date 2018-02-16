3897598e4cdadfd1995dd59ac6e07a5f8a57d712
carmalou <carmenbourlonlong@gmail.com>
2018-02-15 23:05:34 -0600

    show details underneath issue

--- src/issue-item.js
+++ src/issue-item.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 export default class IssueItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            toggle: false
+        };
+    }
+
+    ToggleDetails() {
+        this.setState(function(previousState) {
+            return { toggle: !previousState.toggle }
+        });
+    }
+
     render() {
         if(!this.props.issue) {
             return null;
         }
         return (
-            <li><a href={ this.props.issue.html_url }>{ this.props.issue.title }</a> | { this.props.issue.user.login }</li>
+            <li className="custom-list"><a href={ this.props.issue.html_url }>{ this.props.issue.title }</a>  | <span onClick={ this.ToggleDetails.bind(this) }>details</span> | { this.props.issue.user.login }
+                { this.state.toggle ? <p>{ this.props.issue.body }</p> : null }
+            </li>
         )
     }
 }
