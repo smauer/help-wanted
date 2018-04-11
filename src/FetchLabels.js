a1748394bf448968ff232e177d746e529dc6fe91
carmalou <carmenbourlonlong@gmail.com>
2018-04-10 21:11:21 -0500

    ugh

--- src/FetchLabels.js
+++ src/FetchLabels.js
@@ -1,5 +1,5 @@
 export default function FetchLabels() {
-    return window.fetch('https://api.github.com/repos/techlahoma/help-wanted/labels&per_page=100')
+    return window.fetch('https://api.github.com/repos/techlahoma/help-wanted/labels?per_page=100')
         .then(function(response) {
             return response.json();
         });
