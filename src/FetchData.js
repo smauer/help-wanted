678064b078586af1451e55b1e5ea3a7606472377
carmalou <carmenbourlonlong@gmail.com>
2018-10-20 19:55:45 -0500

    fix markdown issue

--- src/FetchData.js
+++ src/FetchData.js
@@ -1,5 +1,9 @@
 export default function FetchData(link) {
-    return window.fetch(link)
+    return window.fetch(link, {
+        headers: {
+            Accept: 'application/vnd.github.VERSION.html+json'
+        }
+    })
         .then(function(response) {
             return response;
         });
--- src/issue-item.js
+++ src/issue-item.js
@@ -26,7 +26,7 @@ export default class IssueItem extends Component {
                     <a id={ "issue-user-link-" + this.props.issue.id } target="_blank" className="btn btn-link pl-2 py-0" href={this.props.issue.user.html_url }>{ this.props.issue.user.login }</a>
                 </div>                
                 <div className="col text-right">
-                    { this.props.issue.body.length ? (
+                    { this.props.issue.body_html.length ? (
                         <button className="btn btn-secondary mr-3" onClick={ this.ToggleDetails.bind(this) }>
                             { this.state.toggle ? 'Close details' : 'Show details' }
                         </button>
@@ -34,7 +34,7 @@ export default class IssueItem extends Component {
                     }                    
                     <a id={ "issue-link-" + this.props.issue.id } target="_blank" className="btn btn-primary" href={ this.props.issue.html_url }>View on Github</a>
                 </div>
-                { this.state.toggle ? <div id={"issue-body-" + this.props.issue.id } className="p-2 pt-4 border-top border-light w-100 d-block"><p>{ this.props.issue.body }</p></div> : null }
+                { this.state.toggle ? <div id={"issue-body-" + this.props.issue.id } className="p-2 pt-4 border-top border-light w-100 d-block"><p dangerouslySetInnerHTML={ { __html: this.props.issue.body_html } }></p></div> : null }
             </li>
         )
     }
